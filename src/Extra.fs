caf4d3786c1acf1662802a8da27bbc2c48a7bc29
Maxime Mangel <maxime.shadaen@protonmail.com>
2019-10-24 10:35:27 +0200

    Remove Key system from the extraCoder definition as it causes problem for .Net side

--- src/Extra.fs
+++ src/Extra.fs
@@ -7,37 +7,23 @@ let empty: ExtraCoders =
     { Hash = ""
       Coders = Map.empty }
 
-[<StringEnum>]
-type internal InternalCoder =
-    | Int64
-    | Uint64
-    | Decimal
-    | Bigint
-
-type internal Key =
-    | Coder of InternalCoder
-    | NewCoder
-
-let inline internal withCustomAndKey (key: Key) (encoder: Encoder<'Value>) (decoder: Decoder<'Value>)
+let inline internal withCustomAndKey (encoder: Encoder<'Value>) (decoder: Decoder<'Value>)
            (extra: ExtraCoders): ExtraCoders =
     { extra with
-          Hash =
-              match key with
-              | Coder k -> extra.Hash + key.ToString()
-              | NewCoder -> System.Guid.NewGuid().ToString()
+          Hash = System.Guid.NewGuid().ToString()
           Coders =
               extra.Coders |> Map.add typeof<'Value>.FullName (Encode.boxEncoder encoder, Decode.boxDecoder decoder) }
 
 let inline withCustom (encoder: Encoder<'Value>) (decoder: Decoder<'Value>) (extra: ExtraCoders): ExtraCoders =
-    withCustomAndKey NewCoder encoder decoder extra
+    withCustomAndKey encoder decoder extra
 
-let inline withInt64 (extra: ExtraCoders): ExtraCoders = withCustomAndKey (Coder Int64) Encode.int64 Decode.int64 extra
+let inline withInt64 (extra: ExtraCoders): ExtraCoders = withCustomAndKey Encode.int64 Decode.int64 extra
 
 let inline withUInt64 (extra: ExtraCoders): ExtraCoders =
-    withCustomAndKey (Coder Uint64) Encode.uint64 Decode.uint64 extra
+    withCustomAndKey Encode.uint64 Decode.uint64 extra
 
 let inline withDecimal (extra: ExtraCoders): ExtraCoders =
-    withCustomAndKey (Coder Decimal) Encode.decimal Decode.decimal extra
+    withCustomAndKey Encode.decimal Decode.decimal extra
 
 let inline withBigInt (extra: ExtraCoders): ExtraCoders =
-    withCustomAndKey (Coder Bigint) Encode.bigint Decode.bigint extra
+    withCustomAndKey Encode.bigint Decode.bigint extra
