8241c959f1727ce998eed3196fca12aefa645ec9
Maxime Mangel <mangel.maxime@protonmail.com>
2020-01-13 15:01:38 +0100

    Release version 3.5.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 
+## 3.5.0 - 2020-01-13
+
+### Changed
+
+* Upgrade to Fable.Core 3.1.4 and fix warnings
+
 ## 3.4.1 - 2019-10-25
 
 ### Changed
--- nacara.js
+++ nacara.js
@@ -10,7 +10,7 @@ module.exports = {
     editUrl: "https://github.com/thoth-org/Thoth.Json/edit/master/docs",
     title: "Thoth.Json",
     debug: true,
-    version: "3.4.1",
+    version: "3.5.0",
     navbar: {
         showVersion: true,
         links: [
