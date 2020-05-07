0c38aead902fbecd0e3e8a547253253c3ebf639a
Maxime Mangel <mangel.maxime@protonmail.com>
2020-05-08 00:12:22 +0200

    Add quicktests

--- tests/Main.fs
+++ tests/Main.fs
@@ -5,11 +5,28 @@ open Fable.Core
 let [<Global>] describe (name: string) (f: unit->unit) = jsNative
 let [<Global>] it (msg: string) (f: unit->unit) = jsNative
 
+open Thoth.Json
+open Util.Testing
+open Fable.Core.Experimental
+
+let quicktests : Test =
+    testList "QuickTest" [
+        testList "Fake category" [
+            testCase "QuickTest: #1" <| fun _ ->
+                ()
+        ]
+    ]
+
 let run () =
-    let tests = [ Tests.Decoders.tests
-                  Tests.Encoders.tests
-                  Tests.ExtraCoders.tests
-                ] :> Util.Testing.Test seq
+    let tests =
+        [
+            Tests.Decoders.tests
+            Tests.Encoders.tests
+            Tests.ExtraCoders.tests
+            // Uncomment this line if you want to use the quicktests useful
+            // when prototyping or trying to reproduce an issue
+            // quicktests
+        ] :> Util.Testing.Test seq
 
     for (moduleName, moduleTests) in tests do
         describe moduleName <| fun () ->
