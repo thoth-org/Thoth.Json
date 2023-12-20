17dbab3581ccbdfeb62720b301f0ce59d50fb1da
Maxime Mangel <mangel.maxime@protonmail.com>
2023-12-20 17:50:08 +0100

    WIP: Documentation

--- docs/_data/eleventyComputed.js
+++ docs/_data/eleventyComputed.js
@@ -1,11 +1,5 @@
-const path = require("path");
-
 module.exports = {
     permalink: data => {
-        return path.format({
-            ...path.parse(data.page.inputPath),
-            base: undefined,
-            ext: ".html"
-        })
+        return data.page.filePathStem + ".html";
     }
 }
