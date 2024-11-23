c42c1fb3f9ffbab090372b4584b6f85cc44db50e
Maxime Mangel <mangel.maxime@protonmail.com>
2024-11-23 18:45:35 +0100

    chore: update release process to use EasyBuild ecosystem with EasyBuild.ChangelogGen

--- build/Commands/Publish.fs
+++ build/Commands/Publish.fs
@@ -1,16 +1,27 @@
 module EasyBuild.Commands.Publish
 
-open System
 open System.IO
-open Build.Utils
+open EasyBuild.Tools.DotNet
+open EasyBuild.Tools.Git
 open Spectre.Console.Cli
 open EasyBuild.Commands.Test
 open EasyBuild.Workspace
+open SimpleExec
+open BlackFox.CommandLine
 
-let private publish (projectDir: string) =
+let private publish (projectDir: string) (tag: string) =
     printfn $"Publishing {projectDir}"
 
-    let nugetKey = Environment.GetEnvironmentVariable("NUGET_KEY")
+    let changelogPath = Path.Combine(projectDir, "CHANGELOG.md")
+
+    let _ =
+        DotNet.changelogGen (
+            changelogPath,
+            tagFilter = [ tag ],
+            config = Workspace.``commit.config.json``,
+            // Allow dirty working directory because we make a single commit for all packages
+            allowDirty = true
+        )
 
     // Delete the bin folder, so dotnet pack will always create a new package
     // Otherwise, if the package already exists, it will not be created
@@ -20,11 +31,9 @@ let private publish (projectDir: string) =
     if Directory.Exists binFolder then
         Directory.Delete(binFolder, true)
 
-    if isNull nugetKey then
-        failwithf $"Missing NUGET_KEY environment variable"
+    let nupkgPath = DotNet.pack projectDir
 
-    let nupkgPath = Dotnet.pack projectDir
-    Dotnet.Nuget.push (nupkgPath, nugetKey, skipDuplicate = true)
+    DotNet.nugetPush (nupkgPath, skipDuplicate = true)
 
 type PublishSettings() =
     inherit CommandSettings()
@@ -36,10 +45,27 @@ type PublishCommand() =
     override _.Execute(context: CommandContext, settings: PublishSettings) =
         TestCommand().Execute(context, TestSettings()) |> ignore
 
-        publish Workspace.packages.``Thoth.Json``.``.``
-        publish Workspace.packages.``Thoth.Json.Core``.``.``
-        publish Workspace.packages.``Thoth.Json.JavaScript``.``.``
-        publish Workspace.packages.``Thoth.Json.Newtonsoft``.``.``
-        publish Workspace.packages.``Thoth.Json.Python``.``.``
+        publish Workspace.packages.``Thoth.Json``.``.`` "legacy"
+        publish Workspace.packages.``Thoth.Json.Core``.``.`` "core"
+        publish Workspace.packages.``Thoth.Json.JavaScript``.``.`` "javascript"
+        publish Workspace.packages.``Thoth.Json.Newtonsoft``.``.`` "newtonsoft"
+        publish Workspace.packages.``Thoth.Json.Python``.``.`` "python"
+
+        publish
+            Workspace.packages.``Thoth.Json.System.Text.Json``.``.``
+            "system.text.json"
+
+        // To not pollute the git history too much, we make a single commit for all packages
+        Git.addAll ()
+
+        Command.Run(
+            "git",
+            CmdLine.empty
+            |> CmdLine.appendRaw "commit"
+            |> CmdLine.appendPrefix "-m" $"chore: release"
+            |> CmdLine.toString
+        )
+
+        Git.push ()
 
         0
